8fdf0c3fa482f07f489e40beb7f38c300a7b6f12
Trushin Arseniy <a.trushin@hh.ru>
2018-07-25 12:20:23 +0300

    fixed install for python3, build script updated

--- python/gbdtpl/__init__.py
+++ python/gbdtpl/__init__.py
@@ -1,2 +1,7 @@
-from Booster import *
-from DataMat import *
+try:
+    from Booster import *
+    from DataMat import *
+except Exception as ex:
+    print ('python2-3 compatability problem: ', ex)
+    from .Booster import *
+    from .DataMat import *
